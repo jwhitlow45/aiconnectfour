749a9f2fc95a5afcd60586654a8662073ff3b755
Jacob Whitlow <jwhitlow45@gmail.com>
2022-11-03 20:08:07 -0700

    feat:add algorithm to check if coords are in bound and check for win condition in a column

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from src.Board import Board
 
 def main():
     
-    test = Board()
     
+    grid = [
+        ['x','x','x','x','x','0'],
+        ['x','x','x','x','0','x'],
+        ['x','x','x','0','x','x'],
+        ['x','x','0','x','x','x'],
+        ['x','x','1','1','1','0']
+    ]
+    
+    test = Board(grid)
+    
+    print(test.checkWin(2))
     pass
 
 if __name__ == '__main__':
